49c1ce6de2a232af93dece9c1e2ca8e5be9b38f8
Arahasratyan <arahasratyan1@gmail.com>
2026-07-14 13:30:08 +0400

    Solve roc-curve

--- roc-curve/roc-curve.py
+++ roc-curve/roc-curve.py
@@ -3,21 +3,25 @@ import numpy as np
 def roc_curve(y_true, y_score):
     """
     Compute ROC curve from binary labels and scores.
+    Fully vectorized version.
     """
 
-    thresholds = np.array([np.inf] + sorted(list(set(y_score)), reverse=True))
-    y_true, y_score = np.asarray(y_true), np.asarray(y_score)
-    tpr, fpr = [], []
+    y_true = np.asarray(y_true)
+    y_score = np.asarray(y_score)
     
-    for threshold in thresholds:
-        y_score_pr = y_score >= threshold
-        TP = np.sum((y_score_pr==1) & (y_true==1))
-        FP = np.sum((y_score_pr==1) & (y_true==0))
-        TN = np.sum((y_score_pr==0) & (y_true==0))
-        FN = np.sum((y_score_pr==0) & (y_true==1))
-        tpr.append(TP/(TP+FN))
-        fpr.append(FP/(FP+TN))
-        
+    unique_scores = np.unique(y_score)[::-1]
+    thresholds = np.r_[np.inf, unique_scores]
     
-    return np.array(fpr), np.array(tpr), thresholds
+    y_score_pr = y_score[:, None] >= thresholds[None, :]
+    
+    TP = np.sum(y_score_pr & (y_true[:, None] == 1), axis=0)
+    FP = np.sum(y_score_pr & (y_true[:, None] == 0), axis=0)
+    
+    P = np.sum(y_true == 1)
+    N = np.sum(y_true == 0)
+    
+    tpr = TP / P if P > 0 else np.zeros_like(TP, dtype=float)
+    fpr = FP / N if N > 0 else np.zeros_like(FP, dtype=float)
+    
+    return fpr, tpr, thresholds
     
